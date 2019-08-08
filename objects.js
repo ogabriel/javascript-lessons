89e52a07f64d8c162b236fe189c1b2683a65baa7
ogabriel <gabrieloliver8991@gmail.com>
2019-08-08 00:36:36 -0300

    update objects

--- objects.js
+++ objects.js
@@ -7,14 +7,36 @@ var personzinha = {
 document.write(personzinha.name.concat(' || ', personzinha['name'], '\n'));
 
 // constructor
-class person {
-    constructor(name, age, color) {
+function person(name, age, color) {
         this.name = name;
         this.age = age;
         this.fav_color = color;
     }
-}
 
 var pessoinha = new person("Aurelio", 789, 'preto e branco');
 
 document.write(pessoinha.name.concat(' & ', pessoinha.age, ' & ', pessoinha.fav_color));
+
+function person2(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var John = new person2('John', 45);
+var James = new person2('James', 89);
+
+function person3(name, age) {
+    this.name = name;
+    this.age = age;
+    this.yearOfBirth = bornYear;
+}
+
+function bornYear() {
+    return new Date().getFullYear() - this.age;
+    // return 10 - this.age;
+}
+
+var me = new person3('Myself', 20);
+
+// it's a function, must be called as a function
+document.write(me.yearOfBirth());
